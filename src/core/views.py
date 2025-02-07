9a61bc035fa2e762b3fa7e39d972f99231e7ca2e
Marketing <marketing@iMac-de-Marketing.local>
2025-02-07 13:42:55 -0300

    actualizacion de iderp

--- src/core/views.py
+++ src/core/views.py
@@ -4314,8 +4314,8 @@ def procesar_producto(producto, total_productos, index, retry=False):
 def ajustar_stock_bsale(request):
     if request.method != "POST":
         return JsonResponse({"error": "Método no permitido"}, status=405)
-    #productos = list(Products.objects.all())
-    productos = list(Products.objects.order_by('-id')[:1000])
+    productos = list(Products.objects.all())
+    #productos = list(Products.objects.order_by('-id')[:1000])
     total_productos = len(productos)
     print("🔄 Iniciando comparación y ajuste de stock...")
     
